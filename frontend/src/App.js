094f00f8fb6438b22d9aacc869d027f509e7d925
samuelashraff <48100109+samuelashraff@users.noreply.github.com>
2023-10-03 17:39:22 +0300

    Working (and a very simple) prototype of data fetching with firebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { collection, getDocs } from 'firebase/firestore';
+import {db} from './firebase'
+
+import { useEffect, useState } from 'react';
+
 function App() {
+
+  const [events, setEvents] = useState([])
+
+  const fetchEvents = async () => {
+       
+    await getDocs(collection(db, "events"))
+        .then((querySnapshot)=>{               
+            const newData = querySnapshot.docs
+                .map((doc) => ({...doc.data(), id:doc.id }));
+            setEvents(newData);                
+            console.log(events, newData);
+        })
+   
+}
+
+  useEffect(() => {
+    fetchEvents()
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <h2>Events:</h2>
+      <ul>
+        {events.map((event) => {
+          return <li>Name: {event.name}, ID: {event.id}, location: {event.location}</li>
+        })}
+      </ul>
     </div>
   );
 }
