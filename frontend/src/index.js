93a7c1d15e7546a6d0524a52ae0198a1f2fc6155
Samu Ashraff <samuel.ashraff@aalto.fi>
2023-10-03 13:35:01 +0300

    Fixing typo in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { firebaseConfig } from '../firebase';
 
-firebaseConfig.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
